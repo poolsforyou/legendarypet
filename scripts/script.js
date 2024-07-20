ff930367e7de64545552d50c17eaf00dbfc5900b
poolsforyou <79567086+poolsforyou@users.noreply.github.com>
2024-07-20 06:36:49 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -75,8 +75,6 @@ window.setTimeout(function () {
     }
 
     if (chat.indexOf("Your legendary pet finds:") > -1) {
-      console.log("hey");
-      console.log(chat):
       let currentSerenSpiritDetected = chat.match(/\[\d+:\d+:\d+\] Your legendary pet finds: (\d+ x [A-Za-z\s-'()1-4]+)/);
       if (currentSerenSpiritDetected[0].trim() === lastSerenSpiritDetected) {
         return;
